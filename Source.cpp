5aa41e8350f57f8cf5eddb69e8b4ee260aabfb44
Peter <126282015+GDev-Peter@users.noreply.github.com>
2023-10-06 23:05:31 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,167 +1,66 @@
 #include <iostream>
-#include <fstream>
-#include <sstream> // for string streams
-#include <string> // for string
+#include <string>
+#include <cstdlib>
+
+// class game variable
+std::string story           [100]    = {
+                                        ("\nYou are the leader of the biggest underground resistance army. How did you come to this position? Assassination, bribery, stealing and many other organized crimes. Now that you are powerful enough, you start thinking about a revolution. So, you start planning and it all culminates now, with the meeting of all the higher-ups in your underground organization.\n\n(voices in the crowd): Silence! The leader is approaching!\n(All people stop speaking and they all start looking at the door. They were right, the leader was coming.)\n\nGENERAL: Dear Leader, I am glad you are here. We need to sort out the plans for the revolution. We have been talking here for hours and we can't get to decide on one. You need to decide.\nRIGHT-HAND-MAN: I have placed the plans folder on the desk in your office. If you didn't have the time to read them, we will recap them for you.\n\nYOU: Actually, no. I didn't had time to read any of them. I had to stay all night awake to..., you know..., read, and write... stuff.\n\nGENERAL: Then no problem\n\n\n(1) CONTINUE\n"),
+                                        ("\nMARKUS: After our yesterday meeting, me and my fellow patriots marked a plan that involves a lot of bravery.\n\nYOU: Sounds good Markus. What were your people's thoughts?\n\nMARKUS: Well..., we think that it is better to play it safe and talk with the Prime Minister. Maybe he will understand our point...\n\nRIGHT-HAND-MAN: THAT'S ABSURD! Leader, I contest that plan. He only wants to distroy us from the inside. I told you that we shouldn't...\n\nGENERAL: That's enought Carlos. We should calm down. But really Sir Leader, it is stupid to think that they will accept our 15% wage raise and the abolishment of the monarchic system.\n\nYOU: I guess they have a point. Anyone else with a better plan?\n\n(1) CONTINUE\n"),
+                                        ("\nGENERAL: Well sir leader sir, I have also made some plans with the other veterans and non-partisan leaders. We clearly decided that we couldn't say on the side much longer. We need to act sometimes, or we will be discovered. I believe that we could organise some sort of general strike, like that years ago. The difference is that we won't have a horrible death.\n\nYOU: Good idea. Solid idea. Good general. As always. Anyone else?\n\n(1) CONTINUE\n"),
+                                        ("\nRIGHT-HAND-MAN: Well it's obvious that we are in a room full of stupid people. WE CAN'T WAIT ANY LONGER. I have INFORMATION that they are onto us. Leader, we could rely on my soldiers and we could do any job possible.\n\nMARKUS: Sir Leader, stop this lunatic. He is trying to sabotaje our unity. He can't be trusted!\n\nYOU: Stop you too. I unterstand the point of our comarade Carlos, but we should not be to reckless.\n\nGENERAL: Does anyone have something to speek before the leader will chose a glorious plan?\n\n(Pause)\n\nYOU: Well then I will chose.\n\n(1) The first plan\n(2) The second plan\n(3) The third plan\n"),
+                                        ("\nYOU: I chose the....\nGUY: SIR! THE PM WAS ASASINATED!\n\nGENERAL: Horible!\n\nRIGHT-HAND-MAN: Crule!\n\nMARKUS: Damn! My plan is now imposible to achive! NOOO! My GLORY! My PoWeRrrr!!\n\nYOU: Oh, then I must change my choice. It will be...\n\n(1) The second plan\n(2) The third plan\n")
+                                        };
+     int    choices         [100]    = {1,1,1,3,1};
+     int    destination     [100][4] = {
+                                        {1},
+                                        {2},
+                                        {3},
+                                        {4, 100},
+                                        {5}
+                                       };
 
 
+// class main variable
+     int    i                            ;
+     int    decision                     ;
 
-
-bool gameStart = 0;
-bool gameStandby = 1;
-bool gameEnd = 0;
-int complete = 0; // Get the last variable
-short int AuxChoise = 0; // Get the choise
-std::string savetext;
-bool aux;
-bool aux2;
-
-short int chosenplan;
-
-
-bool democracy;
-bool supremacy;
-short int strike_organisation = 0;
-short int randomNumber;
-
-// Charter Y - Etc Function
-void clear_screen(char fill = ' ') {
-	
+void game();
+void clearscreen(){
+    #ifdef WINDOWS
+    std::system("cls");
+    #else
+    std::system ("clear");
+    #endif
 }
 
-// Charter V
+int main(){
 
-void Fail() {
-	if (gameEnd == 1) {
-		return;
-	}
+    std::cout << "Write 1 to continue";
+    std::cin >> decision;
+    if(decision == 1) game();
 }
 
-// Charter A Complete
-
-void Asasination() {
-	std::cout << "(Phone - Assasin) - We have the Right Hand Men cornered, shall we continue?\n(You) - Yes, do what is necessary!\n(Phone) - Aaaaa! aaaa! Ahahaha! Ah... Aaaaa!\n\n";
-	std::cout << "(Phone - Right Hand Men) - You can't kill me! I'm GOD, DESTROYER OF WORLDS!\n";
-	gameEnd = 1;
-	Fail();
+void game(){
+    clearscreen();
+    while(true){
+        std::cout << story[i];
+        std::cin  >> decision;
+        switch (choices[i]){
+            case 1:
+                if(decision == 1) i=destination[i][0];
+                break;
+            case 2:
+                if(decision == 1) i=destination[i][0];
+                if(decision == 2) i=destination[i][1];
+                break;
+            case 3:
+                if(decision == 1) i=destination[i][0];
+                if(decision == 2) i=destination[i][1];
+                if(decision == 3) i=destination[i][2];
+                break;
+        }
+        clearscreen();
+        std::cout << i << " ";
+    }
 }
 
-void Game2() {
-
-	std::cout << "(General) - Boss, now that we are decided about plans, we need to take the next course of action.\n\n";
-	std::cout << " (1) > Right Hand Man has been very suspicious lately. We need to get him asasinated. Recruit later!\n";
-	std::cin >> AuxChoise;
-	if (AuxChoise == 1) {
-		Asasination();
-	}
-	else {
-		std::cin >> AuxChoise;
-	}
-}
-void Game3() {
-	clear_screen();
-	std::cout << "(The general left the room, and, the Leader is waiting for the arival for his right hand man. He will be waiting for a long time) \n";
-}
-void GAME1() {
-	// Local Variable
-	bool makechoise = false;
-	AuxChoise = 0;
-	// Introduction
-	while (aux == false) {
-		switch (AuxChoise) {
-		case 0:
-			std::cout << "temp, chose betwen 1,2,3,4";
-			clear_screen();
-			std::cin >> AuxChoise;
-			break;
-		case 1:
-			clear_screen();
-			std::cout << " (Markus, leader of liberal party in exile) - We heared your plan the other day and we wrote it up. It is a good idea to talk with the prime-minister. It is sympathetic with us. He may help us in the revolution!\n\n";
-			std::cout << " (2) > Now that I know everything about my plan, I wish to talk with the General about his plan.\n";
-			std::cout << " (3) > My plan is briliant. Right Hand Men, can you remind me how your plan was?\n";
-			std::cout << " (4) > Now that I know the details of my plan, I will make the final decision!\n";
-			std::cin >> AuxChoise;
-			break;
-		case 2:
-			clear_screen();
-			std::cout << " (General) - Your plan is glorious, but I don't think the prime minister will cooperate, due to his friendship with the emperor. We shall need to organise our militia and take power by popular force!\n\n";
-			std::cout << " (3) > My plan is briliant and the General's is inteligent. Right Hand Men, what do you have to say?\n";
-			std::cout << " (4) > Of curse, we need a backup plan. I don't care about the Right Hand Man's plan. I will make my final decision!\n";
-			std::cin >> AuxChoise;
-			break;
-		case 3:
-			clear_screen();
-			std::cout << " (Right Hand Man) - We don't have plan for diplomacy, we need force, fast force. Decisive force. We need to imidiatly destroy the empire!\n\n";
-			std::cout << " (4) > Of curse, like you say.... . They are all ok plans. I will now make my final decision!\n";
-			std::cin >> AuxChoise;
-			break;
-		case 4:
-			clear_screen();
-			aux = true;
-			AuxChoise = 0;
-			break;
-		default:
-			std::cin >> AuxChoise;
-			break;
-		}
-		
-	}
-
-	// Chose a plan
-	while (aux == true) {
-		AuxChoise = 0;
-		std::cout << " (You) - I have decided the final plan. The best. The great. I shall be\n\n";
-		std::cout << " (1) > MY PLAN!\n";
-		std::cout << " (2) > GENERAL'S PLAN!\n";
-		std::cout << " (3) > RIGHT HAND MAN'S PLAN!\n";
-		std::cin >> AuxChoise;
-		if (AuxChoise == 1 || AuxChoise == 2) {
-			clear_screen();
-			std::cout << " (General) - We need to take a decision. What should we do. I mean, neither me or you had a perfect plan. We need to check the details.\n(Soldier) - Dear Leader! General! Bad news! Word that the curent prime minister is... death!\n(General) - Well, this is inconvinient. Boss, how we shall procede?\n\n";
-			std::cout << " (1) > This is a problem, but are able to overcome this. General, please continue with your plan!\n";
-			std::cout << " (2) > We were dumb in our racionament. We need to talk with the Right Hand Men, he was in the right!\n";
-			aux = false;
-			std::cin >> AuxChoise;
-			if (AuxChoise == 1) {
-				clear_screen();
-				chosenplan = 2;
-				Game2();
-			}
-			else if (AuxChoise == 2){
-				clear_screen();
-				chosenplan = 3;
-				Game3();
-			}
-			else {
-				std::cin >> AuxChoise;
-			}
-			
-		}
-		else if (AuxChoise == 3) {
-			savetext += std::to_string(AuxChoise);
-			std::ofstream gamePlayFile("savefile.txt");
-			gamePlayFile << AuxChoise;
-			gamePlayFile.close();
-			chosenplan = 3;
-			aux = false;
-			Game3();
-		}
-		else {
-			std::cin >> AuxChoise;
-		}
-	}
-}
-
-
-int main() {
-	/* ********** BEFORE-GAME ********** */
-	srand(time(0));
-
-	std::cout << "Type 1 to continue\n";
-	std::cin >> AuxChoise;
-	if (AuxChoise = 1)
-	{
-		GAME1();
-	}
-	
-	std::cin >> AuxChoise;
-}
